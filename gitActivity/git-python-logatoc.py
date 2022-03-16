e6a852c7ba9e90d7de8b0dbe7df0800f7d3036a1
LogatocRialyn <logatocrialyn>
2022-03-15 13:49:27 -1200

    My Educational Background Added

--- gitActivity/git-python-logatoc.py
+++ gitActivity/git-python-logatoc.py
@@ -8,3 +8,6 @@ Created on Tue Mar 15 13:31:04 2022
 print("Rialyn M. Logatoc")
 print("Candahon Mogpog Marinduque")
 print("July 10 2001")
+print("Marinduque State College")
+print("Bachelor of Science in Inforamtion Technology")
+print("BSIT 2C")
